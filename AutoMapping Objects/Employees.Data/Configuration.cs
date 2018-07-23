b4d674581c9d8e5b5ad5f973701a6d1a4b216f31
Georgi Popov <popov937@abv.bg>
2018-07-23 12:05:19 +0300

    Fix CONNECTION_STRING

--- AutoMapping Objects/Employees.Data/Configuration.cs
+++ AutoMapping Objects/Employees.Data/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public const string CONNECTION_STRING = @"Server=DESKTOP-11B1NH5\SQLEXPRESS; Database=EmployeesDb; Integrated Security=true";
+        public const string CONNECTION_STRING = @"Server=.; Database=EmployeesDb; Integrated Security=true";
     }
 }
